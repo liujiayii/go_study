b289ad1f9037d9bf234a0bad1f61fb105b2dfdb9
LIUJIAYI <liujiayii@foxmail.com>
2020-03-20 22:29:11 +0800

    注释

---
+++ src/go_code/chapter02/escaptechar/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+	fmt.Println("tom\tjack")
+	fmt.Println("hello\nworld")
+	fmt.Println("tom说\"i love you\"")
+	fmt.Println("天龙八部\r张飞")
+}
